8bc263455eef4a111c52fe8c75b2828dcfc9a45e
Ross Coundon <ross.coundon@leadentsolutions.com>
2022-05-11 22:52:37 +0100

    Use functional stack approach

--- stacks/index.ts
+++ stacks/index.ts
@@ -1,13 +1,13 @@
-import GarageDoorStack from "./GarageDoorStack";
-import * as sst from "@serverless-stack/resources";
+import { GarageDoorStack } from './GarageDoorStack';
+import { App } from '@serverless-stack/resources';
 
-export default function main(app: sst.App): void {
+export default function main(app: App): void {
   // Set default runtime for all functions
   app.setDefaultFunctionProps({
-    runtime: "nodejs14.x",
+    runtime: 'nodejs14.x',
     architecture: 'arm_64',
-    memorySize: 256
+    memorySize: 256,
   });
 
-  new GarageDoorStack(app, "garage-door-stack");
+  app.stack(GarageDoorStack, { id: 'garage-door-stack' });
 }
