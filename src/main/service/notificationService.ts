0d45ff513d102be7dd797ce92e44babb604fdc38
Ross Coundon <ross.coundon@leadentsolutions.com>
2022-05-12 08:11:25 +0100

    Update notification msg

--- src/main/service/notificationService.ts
+++ src/main/service/notificationService.ts
@@ -3,7 +3,7 @@ import { TopicClient } from '@/transit/aws/sns/TopicClient';
 import { DoorState } from '@/handlers/lambda';
 
 export const notify = async (state: DoorState) => {
-  const text = `Door state changed to ${state}`;
+  const text = state === 'open' ? `I think you might have left the door open!` : `Door closed`;
   await EmailSender.sendEmail(text, state, [process.env.EMAIL_TARGET]);
   await TopicClient.publishMessage(text, process.env.TOPIC_ARN);
 };
