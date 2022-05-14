61e1d751db19c497a766da532de9a15b0bb7eefc
Ross Coundon <ross.coundon@leadentsolutions.com>
2022-05-14 11:52:13 +0100

    Use node 16!

--- stacks/index.ts
+++ stacks/index.ts
@@ -4,7 +4,7 @@ import { App } from '@serverless-stack/resources';
 export default function main(app: App): void {
   // Set default runtime for all functions
   app.setDefaultFunctionProps({
-    runtime: 'nodejs14.x',
+    runtime: 'nodejs16.x',
     architecture: 'arm_64',
     memorySize: 256,
   });
